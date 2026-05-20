a892b1476e5b60104da4a7fa0560758aa44fc659
JorgeTSW <jorge.pedroza@tecdesoftware.edu.mx>
2026-05-20 12:16:17 -0600

    CREATE: Domain - User, Review

---
+++ Catalogo.Domain/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalogo.Domain.Models;
+
+public class Review
+{
+    public int Id { get; set; }
+    public int ItemId { get; set; }
+    public int UserId { get; set; }
+    public string NombreUser { get; set; } = string.Empty;
+    public string Comentario { get; set; } = string.Empty;
+    public int Rating { get; set; } // 1 a 5
+}
---
+++ Catalogo.Domain/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalogo.Domain.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
